7c6548626a159af75002467876435924f1cc8ceb
asapcatftw <31966895+asapcatftw@users.noreply.github.com>
2019-11-14 15:41:54 -0500

    Docker + Docker-Compose
    Install script for Docker+Docker-compose

---
+++ docker/docker-with-compose.sh
@@ -0,0 +1,9 @@
+echo "Docker Install script"
+curl -fsSL https://get.docker.com/ | sh
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $(whoami)
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker version
+docker-compose --version
