ecc65f663019cf87f63402904c929c5f27045b5d
asapcatftw <31966895+asapcatftw@users.noreply.github.com>
2023-06-28 12:32:20 -0400

    Update docker-with-compose.sh

--- docker/docker-with-compose.sh
+++ docker/docker-with-compose.sh
@@ -3,7 +3,7 @@ curl -fsSL https://get.docker.com/ | sh
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker $(whoami)
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker version
 docker-compose --version
