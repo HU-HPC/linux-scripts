49bf6e0f7488a1483d765bb90b41c15a0d56f769
asapcatftw <31966895+asapcatftw@users.noreply.github.com>
2024-01-17 20:52:49 -0500

    Create setup.sh

---
+++ docker-registry/setup.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+cat << "EOF"
+
+
+ /$$   /$$ /$$   /$$       /$$   /$$ /$$$$$$$   /$$$$$$ 
+| $$  | $$| $$  | $$      | $$  | $$| $$__  $$ /$$__  $$
+| $$  | $$| $$  | $$      | $$  | $$| $$  \ $$| $$  \__/
+| $$$$$$$$| $$  | $$      | $$$$$$$$| $$$$$$$/| $$      
+| $$__  $$| $$  | $$      | $$__  $$| $$____/ | $$      
+| $$  | $$| $$  | $$      | $$  | $$| $$      | $$    $$
+| $$  | $$|  $$$$$$/      | $$  | $$| $$      |  $$$$$$/
+|__/  |__/ \______/       |__/  |__/|__/       \______/
+
+---------------- Docker Registry script ----------------
+
+apt install curl -y
+
+mkdir data
+
+wget https://raw.githubusercontent.com/HU-HPC/linux-scripts/master/docker-registry/config.yml
+wget https://raw.githubusercontent.com/HU-HPC/linux-scripts/master/docker-registry/docker-compose.yml
+
+echo "Run the following command to start docker registry!"
+echo "docker compose up -d"
